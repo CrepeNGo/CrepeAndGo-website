a2829517545a28d0ec1d417c69f3c60784ea9558
Molky <32463747+Molky-dev@users.noreply.github.com>
2023-04-12 16:47:26 +0200

    aaaaa

--- photos/view/login/login.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="http://<?=$HTTP_SERVER_VARS['HTTP_HOST']?>/style.css">
-
-<form action="/login/login" method="POST">
-    <label for="">username</label>
-    <input type="text" name='username'>
-    <label for="">mot de passe</label>
-    <input type="text" name='mdp'>
-    <button type="submit">Confirmer</button>
-</form>
